b26218a19cf9da24d454e1320ebc9a80d4f4d2c8
Ardiles <ardiles.montenegro@gmail.com>
2019-08-07 17:55:45 -0300

    Extrai os CPFs e cria senhas criptografadas.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const csv = require('csv-parser')
+const csv = require('fast-csv')
+const bcrypt = require('bcryptjs')
 const fs = require('fs')
+const ws = fs.createWriteStream("resultado.csv")
 
 fs.createReadStream('arquivo.csv')
-  .pipe(csv())
+  .pipe(csv.parse({ headers: true }))
   .on('data', (row) => {
-    console.log(row)
+    const cpf = row.cpf
+    const encryptedPassword = bcrypt.hashSync(row.cpf, 11)
+    //console.log(row)
+    console.log(`CPF: ${cpf} - Senha: ${encryptedPassword}`)
   })
   .on('end', () => {
-    console.log('CSV file successfully processed')
+    console.log('Arquivo lido com sucesso!')
+    /* csv
+      .write(data, { headers: true })
+      .pipe(ws) */
   })
